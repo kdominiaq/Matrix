01064594f9c0b3607a4083ee0e9d7d22a6a69328
Kacper Dominiak <69914937+kdominiaq@users.noreply.github.com>
2021-08-15 22:12:03 +0200

    Final Version :)

--- main.cpp
+++ main.cpp
@@ -4,16 +4,25 @@
 
 int main() {
 
-    Matrix<double> a(2,7);
-    Matrix<double> b(7, 2);
-    Matrix<double> c;
+    Matrix<double> a(3, 6);
+    Matrix<double> b(6, 3);
+    Matrix<double> z;
 
-    a.init_value_by_user();
+    a.init_value();
     b.init_value();
 
+    std::cout << "Matrix A" << std::endl;
+    a.print();
+    std::cout << "Matrix B" << std::endl;
+    b.print();
 
-    c = Matrix<double>::compute_grad(a,b);
-    c.print();
+    std::cout << "The result of matrix multiplication: A*B" << std::endl;
+    z = a * b;
+    z.print();
+
+    std::cout << "The result of derivative matrix with respect to matrix: dZ/dA,  Z = A*B" << std::endl;
+    z = Matrix<double>::compute_grad(a, b);
+    z.print();
 
     return 0;
 }
