aaf6b3efb7aa29dfb791cdbae6cf89dc1077bffa
Kacper Dominiak <69914937+kdominiaq@users.noreply.github.com>
2021-08-15 20:12:09 +0200

    Final Version

--- main.cpp
+++ main.cpp
@@ -4,22 +4,15 @@
 
 int main() {
 
-    srand(time(NULL));
+    Matrix<double> a(2,7);
+    Matrix<double> b(7, 2);
+    Matrix<double> c;
 
+    a.init_value_by_user();
+    b.init_value();
 
-    Matrix<double> a(3,2);
-    Matrix<double> b(2, 3);
-    a.rand();
-    a.print();
-    b.rand();
-    b.print();
-    Matrix<double> c(2, 2);
-    c =a*b;
-
-    c.print();
 
     c = Matrix<double>::compute_grad(a,b);
-
     c.print();
 
     return 0;
