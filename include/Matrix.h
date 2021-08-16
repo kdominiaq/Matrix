e63909e7fee4e3e68b6297f19d13798bb0a8ae2a
Kacper Dominiak <69914937+kdominiaq@users.noreply.github.com>
2021-08-16 17:41:25 +0200

    Update Matrix.h

--- include/Matrix.h
+++ include/Matrix.h
@@ -12,65 +12,73 @@
 
 template <typename T>
 class Matrix
-{
-private:
-    std::vector<std::vector<T>> data_;
+        {
+        private:
+            std::vector<std::vector<T>> data_;
 
-    unsigned int rows_{};
-    unsigned int cols_{};
-public:
+            unsigned int rows_{};
+            unsigned int cols_{};
+        public:
 
-    /**
-     * Default constructor without parameters
-     */
-    Matrix<T>() = default;
+            /**
+             * Default constructor without parameters
+             */
+            Matrix<T>() = default;
 
-    /**
-     * Creating a Matrix by rows and cols values
-     *
-     * @param rows Number of rows in the matrix
-     * @param cols Number of columns in the matrix
-     */
-    Matrix<T>(unsigned int rows, unsigned int cols);
-
-    /**
-     * Matrix Multiplication by operator *, number of columns in the first matrix must be equal to number of rows
-     * in the "rhs" Matrix
-     *
-     * @param rhs Second matrix to multiply
-     * @return Matrix multiplication result
-     */
-    Matrix<T> operator*(Matrix<T>& rhs);
+            /**
+             * Creating a Matrix by rows and cols values
+             *
+             * @param rows Number of rows in the matrix
+             * @param cols Number of columns in the matrix
+             */
+            Matrix<T>(unsigned int rows, unsigned int cols);
 
 
-    /**
-     * Compute matrix derivative with respect of matrix, dZ/dA and dZ/dB -> Z = A * B;
-     *
-     * @param A The first Matrix to multiply and calculations dZ/dA
-     * @param B The second Matrix to multiply and calculations dZ/dB
-     * @return Matrix dZ/dA
-     */
-    static Matrix<T> compute_grad(Matrix<T>& A, Matrix<T>& B);
+            /**
+             *  Clearing memory
+             */
+            ~Matrix<T>();
 
 
+            /**
+             * Matrix Multiplication by operator *, number of columns in the first matrix must be equal to number of rows
+             * in the "rhs" Matrix
+             *
+             * @param rhs Second matrix to multiply
+             * @return Matrix multiplication result
+             */
+            Matrix<T> operator*(Matrix<T>& rhs);
 
-    /**
-     * Fills the matrix with values from user
-     */
-    void init_value_by_user();
 
-    /**
-     * Fills the matrix with values
-     */
-    void init_value();
-
-    /**
-     * Prints the matrix
-     */
-    void print();
+            /**
+             * Compute matrix derivative with respect of matrix, dZ/dA or dZ/dB -> Z = A * B;
+             *
+             * @param A The first Matrix to multiply and calculations dZ/dA
+             * @param B The second Matrix to multiply and calculations dZ/dB
+             * @param derivative_by_first_matrix Select Matrix for derivative, if True "dZ/dA" if False "dZ/dB"
+             * @return Matrix dZ/dA or dZ/B
+             */
+            static Matrix<T> compute_grad(Matrix<T>& A, Matrix<T>& B, bool derivative_by_first_matrix = true);
 
 
 
-};
+            /**
+             * Fills the matrix with values from user
+             */
+            void init_value_by_user();
+
+            /**
+             * Fills the matrix with values
+             */
+            void init_value();
+
+            /**
+             * Prints the matrix
+             */
+            void print();
+
+
+
+        };
 
 #endif //MATRIX_MATRIX_H
